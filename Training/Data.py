ea6afba2f007f7545dcfc04bb0b42a55855295b9
SomeshRampure <srampure@26gmail.com>
2025-08-19 08:05:54 +0530

    sum of 1 and 3 is 4

--- Training/Data.py
+++ Training/Data.py
@@ -3,3 +3,13 @@ print("Hello world")
 print("Hello world")
 print("Hello world")
 print("Hello world")
+
+num1=int(input("Enter any number1 : "))
+num2=int(input("Enter any number2 : "))
+sum=num1+num2 
+print(f"sum of {num1} and {num2} is {sum}")
+
+num1=1
+num2=3
+sum=num1+num2 
+print(f"sum of {num1} and {num2} is {sum}")
